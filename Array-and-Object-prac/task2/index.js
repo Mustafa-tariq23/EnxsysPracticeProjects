810c9bf45b2c13590732745069e1a0b86c7c1c22
Enxsys <enxsys@Enxsyss-MacBook-Pro.local>
2024-05-02 15:14:11 +0500

    delete and add index functionality

---
+++ Array-and-Object-prac/task2/index.js
@@ -0,0 +1,202 @@
+let amRoutes = {
+  BSH10A: [
+    {
+      id: "cd3e046d-3037-4b26-af84-0b5d0c746a12",
+      createdAt: "2024-04-29T07:59:17.541Z",
+      updatedAt: "2024-04-29T07:59:17.541Z",
+      lat: "40.0878117",
+      lng: "-74.1816913",
+      formattedAddress: "Ocean Ave &, & Natures Way, Lakewood, NJ 08701, USA",
+      address: "Ocean Ave@NATURES WAY@BOULDER WAY",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "7:56 AM",
+      routeName: "BSH10A",
+    },
+
+    {
+      id: "6a6fbd84-d0b8-45e6-adac-f7d50db5c867",
+      createdAt: "2024-04-29T07:55:43.315Z",
+      updatedAt: "2024-04-29T07:55:43.315Z",
+      lat: "40.0924437",
+      lng: "-74.1958943",
+      formattedAddress: "& Linden Avenue, Lakewood, NJ 08701, USA",
+      address: "Linden Ave@Stirling Ave",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      // time: "7:59 AM",
+      time: "7:56 AM",
+      routeName: "BSH10A",
+    },
+    {
+      id: "cd3e046d-3037-4b26-af84-0b5d0c746a12",
+      createdAt: "2024-04-29T07:59:17.541Z",
+      updatedAt: "2024-04-29T07:59:17.541Z",
+      lat: "40.0878117",
+      lng: "-74.1816913",
+      formattedAddress: "Ocean Ave &, & Natures Way, Lakewood, NJ 08701, USA",
+      address: "Ocean Ave@NATURES WAY@BOULDER WAY",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "7:56 AM",
+      routeName: "BSH10A",
+    },
+    {
+      id: "ffd3d2f6-d19a-4230-89bf-1dceec178cbd",
+      createdAt: "2024-04-29T08:00:42.415Z",
+      updatedAt: "2024-04-29T08:00:42.415Z",
+      lat: "40.096693",
+      lng: "-74.198919",
+      formattedAddress: "& Ridge Avenue, Lakewood, NJ 08701, USA",
+      address: "Ridge Ave@New York Ave",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "8:02 AM",
+      routeName: "BSH10A",
+      timeConflict: true,
+    },
+    {
+      id: "6a6fbd84-d0b8-45e6-adac-f7d50db5c867",
+      createdAt: "2024-04-29T07:55:43.315Z",
+      updatedAt: "2024-04-29T07:55:43.315Z",
+      lat: "40.0924437",
+      lng: "-74.1958943",
+      formattedAddress: "& Linden Avenue, Lakewood, NJ 08701, USA",
+      address: "Linden Ave@Stirling Ave",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "7:59 AM",
+      routeName: "BSH10A",
+    },
+  ],
+
+  BB3A: [
+    {
+      id: "e9ed58c0-922a-434b-a9a0-fb467b8b6e22",
+      createdAt: "2024-04-29T07:55:44.266Z",
+      updatedAt: "2024-04-29T07:55:44.266Z",
+      lat: "40.1147825",
+      lng: "-74.2544899",
+      formattedAddress: "& Pawnee Road, Lakewood, NJ 08701, USA",
+      address: "Pawnee Rd@Mohawk Pl",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "8:30 AM",
+      routeName: "BB3A",
+    },
+    {
+      id: "e9ed58c0-922a-434b-a9a0-fb467b8b6e22",
+      createdAt: "2024-04-29T07:55:44.266Z",
+      updatedAt: "2024-04-29T07:55:44.266Z",
+      lat: "40.1147825",
+      lng: "-74.2544899",
+      formattedAddress: "& Pawnee Road, Lakewood, NJ 08701, USA",
+      address: "Pawnee Rd@Mohawk Pl",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "8:30 AM",
+      routeName: "BB3A",
+    },
+    {
+      id: "e9ed58c0-922a-434b-a9a0-fb467b8b6e22",
+      createdAt: "2024-04-29T07:55:44.266Z",
+      updatedAt: "2024-04-29T07:55:44.266Z",
+      lat: "40.1147825",
+      lng: "-74.2544899",
+      formattedAddress: "& Pawnee Road, Lakewood, NJ 08701, USA",
+      address: "Pawnee Rd@Mohawk Pl",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "8:30 AM",
+      routeName: "BB3A",
+    },
+    {
+      id: "6439be53-806b-4d3b-9760-7f1a00b2aa68",
+      createdAt: "2024-04-29T08:00:57.079Z",
+      updatedAt: "2024-04-29T08:00:57.079Z",
+      lat: "40.1052713",
+      lng: "-74.2278538",
+      formattedAddress: "& Laurelwood Avenue, Lakewood, NJ 08701, USA",
+      address: "Laurelwood Ave@Tuxedo Ter",
+      city: "Lakewood",
+      state: "NJ",
+      zip: "08701",
+      coordinateID: null,
+      isIntersection: true,
+      isValidated: true,
+      time: "8:43 AM",
+      routeName: "BB3A",
+    },
+  ],
+};
+
+function compareForTime(Route) {
+  for (let i = 0; i < Route.length; i++) {
+    for (let j = 0; j < Route.length; j++) {
+      if (i == j) continue;
+      if (
+        Route[i].time === Route[j].time &&
+        Route[i].address === Route[j].address
+      ) {
+        Route.splice(j, 1);
+      }
+    }
+  }
+  console.log(Route.length);
+}
+
+function addIndex(Route) {
+  for (let i = 0; i < Route.length; i++) {
+    for (let j = i+1; j < Route.length; j++) {
+      if (
+        Route[i].time === Route[j].time &&
+        Route[i].address !== Route[j].address
+      ) {
+        Route[i].reviewRequired = true;
+      }
+      else{
+        Route[i].reviewRequired = false;
+      }
+    }
+  }
+  console.log(Route.length);
+}
+// compareForTime(amRoutes.BSH10A);
+// compareForTime(amRoutes.BB3A);
+
+
+
+addIndex(amRoutes.BSH10A);
+addIndex(amRoutes.BB3A);
